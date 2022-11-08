dcbdb329cab01107929fabf6f1dcf9d3f0015b97
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2022-11-08 10:24:16 +0700

    add code template java files

---
+++ javafundamental/src/main/java/com/bcafinance/training/day2.java
@@ -0,0 +1,14 @@
+package com.bcafinance.training;
+
+/*
+@Author Azim a.k.a. Rahmad Nur Azim
+Principle Architect
+Created with Eclipse version 4.25.0
+Created on 08-11-2022 10:23:29
+Last Modified on 08-11-2022 10:23:29
+Version 1.0
+*/
+
+public class day2 {
+
+}
